a0c5d26614309680d79c0bc25061058b2661ef19
Angel Ignissi <angel.is.hatari@gmail.com>
2023-07-04 18:42:50 +0300

    added settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 
+from settings import Settings
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры"""
@@ -9,11 +10,11 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает трудовые ресурсы"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                              self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
-        # назначение цвета фона
-        self.bg_color = (100, 255, 230)
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -25,7 +26,7 @@ class AlienInvasion:
                     sys.exit()
 
             # При каждом проходе цикла перерисовывается экран
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Отображение последнего прорисованного экрана
             pygame.display.flip()
