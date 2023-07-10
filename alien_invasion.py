59e59092a26f1a3e10c81a8b2a610a44e8642a4c
Angel Ignissi <angel.is.hatari@gmail.com>
2023-07-10 22:35:36 +0300

    made _check_events() static

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,18 +10,21 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает трудовые ресурсы"""
         pygame.init()
+
+        # поверхность
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width,
-                                              self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.width, self.settings.height))
         pygame.display.set_caption('Alien Invasion')
 
-        self.ship = Ship(self)
+        # корабль
+        self.ship = Ship(self)  # здесь главная поверхность передается кораблю в качестве аргумента
 
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
             self._check_events()
             self._update_screen()
+            self.ship.update()
 
     def _update_screen(self):
         # При каждом проходе цикла перерисовывается экран
@@ -31,12 +34,18 @@ class AlienInvasion:
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
 
-    @staticmethod
-    def _check_events():
+    def _check_events(self):
         # цикл событий для отслеживания событий у клавиатуры и мыши
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -4,16 +4,25 @@ import pygame
 class Ship:
     """Класс для управления кораблем"""
     def __init__(self, ai_game):
-        """Ининациализирует корабль (как прямоугольный объект) и задает его начальную позицию"""
+        """Ининициализирует корабль - прямоугольный объект и задает его начальное положение на поверхности"""
+        # поверхность для размещения корабля
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        # Загружает изображение корабля и получает прямоугольник (размеры и положение прямоугольника)
+        # сам корабль
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-        # Каждый новый корабль появится на середине нижней грани поверхности
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.midbottom = self.screen_rect.midbottom  # корабль рисуется на середине нижней грани поверхности
+
+        # Флаг перемещения
+        self.moving_right = False
+
+    def update(self):
+        """Обновляет позицию корабля с учетом флага"""
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         """Рисует корабль в текущей позиции на поверхности"""
+        # blit(что, где)
         self.screen.blit(self.image, self.rect)
